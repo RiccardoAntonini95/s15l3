6e5a8890378386c22a7f245d18776f32687794fe
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-02-14 15:42:51 +0100

    Aggiungere i file di progetto.

---
+++ MultiplexEx/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="MultiplexEx.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Compra il biglietto:</h1>
+    <asp:TextBox ID="TxtName" runat="server"></asp:TextBox>
+    <asp:TextBox ID="TxtSurname" runat="server"></asp:TextBox>
+    <asp:DropDownList ID="ReservationRoom" runat="server">
+        <asp:ListItem runat="server">SALA NORD</asp:ListItem>
+        <asp:ListItem runat="server">SALA EST</asp:ListItem>
+        <asp:ListItem runat="server">SALA SUD</asp:ListItem>    
+    </asp:DropDownList>
+    <asp:DropDownList ID="TicketType" runat="server">
+        <asp:ListItem runat="server">INTERO</asp:ListItem>
+        <asp:ListItem runat="server">RIDOTTO</asp:ListItem>
+    </asp:DropDownList>
+    <asp:Button ID="Button1" runat="server" Text="Prenota" OnClick="Btn_Click"/>
+</asp:Content>
---
+++ MultiplexEx/WebForm1.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Questo codice è stato generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace MultiplexEx
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Controllo TxtName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtName;
+
+        /// <summary>
+        /// Controllo TxtSurname.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtSurname;
+
+        /// <summary>
+        /// Controllo ReservationRoom.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ReservationRoom;
+
+        /// <summary>
+        /// Controllo TicketType.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList TicketType;
+
+        /// <summary>
+        /// Controllo Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
